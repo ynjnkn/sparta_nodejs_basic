90b8d08d35ca3dca0149f4cb5e421c5a0f16d301
ynjnkn <yjun_k@hotmail.com>
2022-02-12 12:51:35 +0900

    Node.js 기초반 3-6 완료

--- routes/goods.js
+++ routes/goods.js
@@ -6,55 +6,20 @@ router.get('/', (req, res) => {
     res.send('This is root page');
 });
 
-// DB를 생성하기 전까지 사용할 더미 데이터 
-const goods = [
-    {
-        goodsId: 4,
-        name: "상품 4",
-        thumbnailUrl:
-            "https://cdn.pixabay.com/photo/2016/09/07/02/11/frogs-1650657_1280.jpg",
-        category: "drink",
-        price: 0.1,
-    },
-    {
-        goodsId: 3,
-        name: "상품 3",
-        thumbnailUrl:
-            "https://cdn.pixabay.com/photo/2016/09/07/02/12/frogs-1650658_1280.jpg",
-        category: "drink",
-        price: 2.2,
-    },
-    {
-        goodsId: 2,
-        name: "상품 2",
-        thumbnailUrl:
-            "https://cdn.pixabay.com/photo/2014/08/26/19/19/wine-428316_1280.jpg",
-        category: "drink",
-        price: 0.11,
-    },
-    {
-        goodsId: 1,
-        name: "상품 1",
-        thumbnailUrl:
-            "https://cdn.pixabay.com/photo/2016/09/07/19/54/wines-1652455_1280.jpg",
-        category: "drink",
-        price: 6.2,
-    },
-];
+router.get('/goods', async (req, res) => {
+    const goods = await Goods.find({});
 
-router.get('/goods', (req, res) => {
     res.json({
-        goods: goods,
+        goods,
     });
 });
 
-router.get('/goods/:goodsId', (req, res) => {
-    // URL로부터 굿즈 아이디 가져오기
+router.get('/goods/:goodsId', async (req, res) => {
     const { goodsId } = req.params;
 
     // filter returns an array
     // [detail] => destructuring assignment of the first or only item of the filtered array
-    const [detail] = goods.filter((item) => item.goodsId === Number(goodsId));
+    const [detail] = await Goods.find({ goodsId: Number(goodsId) });
 
     res.json({
         detail,
